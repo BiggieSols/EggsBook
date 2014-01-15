abc0b3f74d0d5710513e90aaf993f77bc353ab9d
sol <solomon.garger@gmail.com>
2014-01-15 15:23:11 -0800

    create models and controllers and posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 EggsBook::Application.routes.draw do
-  resources :users
+  resources :users do
+    resources :feeds, only: [:get]
+  end
+  
   resource :session, only: [:new, :create, :destroy] 
+
   resources :friend_requests, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
+
+  
+  # may not need this
+  # resources :foods, only: [:create, :destroy]
+  # resources :user_foods, only: [:create, :destroy]
 
   get 'about', to: 'static_pages:about'
   get 'contact', to: 'static_pages:contact'
---
+++ db/migrate/20140115232151_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.references :user
+      t.text :details
+
+      t.timestamps
+    end
+    add_index :posts, :user_id
+  end
+end
