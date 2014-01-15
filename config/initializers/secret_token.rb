8c6189692e185c61411dfe79e58e15b625472fda
sol <solomon.garger@gmail.com>
2014-01-15 09:23:18 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EggsBook::Application.config.secret_token = '75db29a7442a8ecef767b2a8464ad3cf7c8e88f61041a8c1773afdd087496f6557e2334812f11c221dab7618882eabde316dfd6af3148874e3f46e046a65b95c'
