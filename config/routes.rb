c695ae670f5d7fba4aeca03c9ac568546908ae5b
sol <solomon.garger@gmail.com>
2014-01-15 15:13:48 -0800

    create static pages controller and corresponding pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ EggsBook::Application.routes.draw do
   resource :session, only: [:new, :create, :destroy] 
   resources :friend_requests, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
+
+  get 'about', to: 'static_pages:about'
+  get 'contact', to: 'static_pages:contact'
 end
