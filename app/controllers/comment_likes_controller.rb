1b0c11f2d14417796966f13ce787368bfc850745
sol <solomon.garger@gmail.com>
2014-01-16 16:56:48 -0800

    show liking users and enable like on comment and post on newsfeed

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -2,12 +2,14 @@ class CommentLikesController < ApplicationController
   def create
     comment_id_to_add = [params[:comment_id].to_i]
     current_user.liked_comment_ids += comment_id_to_add
-    redirect_to user_feed_url(current_user)
+    head :ok
+    # redirect_to user_feed_url(current_user)
   end
 
   def destroy
     comment_id_to_destroy = [params[:comment_id].to_i]
     current_user.liked_comment_ids -= comment_id_to_destroy
-    redirect_to user_feed_url(current_user)
+    head :ok
+    # redirect_to user_feed_url(current_user)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   has_many :likes, as: :likable_object
+  has_many :liking_users, through: :likes, source: :user
 
   attr_accessible :details
 end
