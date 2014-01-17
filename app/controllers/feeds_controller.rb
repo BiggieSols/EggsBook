797c5da0e3bfed705c00cd76c1187135f734d220
sol <solomon.garger@gmail.com>
2014-01-16 17:43:41 -0800

    update what is displayed in feed - show all own and friends posts, liked posts, posts of liked comments

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,10 @@
 class FeedsController < ApplicationController
   def show
     @feed_posts = current_user.friends.map(&:posts).flatten
+    @feed_posts += current_user.posts
+    @feed_posts += current_user.liked_posts
+    @feed_posts.flatten.uniq
+    @feed_posts.sort_by! {|post| post.updated_at }
     # render json: @feed_posts
   end
 end
