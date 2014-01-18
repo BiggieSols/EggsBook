6810e2c3c6615a2064ad380db4872819c2c5501b
Sol <solomon.garger@gmail.com>
2014-01-18 11:43:37 -0800

    add liking_users to posts api

--- app/assets/javascripts/models/post.js
+++ app/assets/javascripts/models/post.js
@@ -2,6 +2,7 @@ EggsBook.Models.Post = Backbone.Model.extend({
   parse: function(response) {
     response.comments = new EggsBook.Collections.Comments(response.comments);
     response.user = new EggsBook.Models.User(response.user);
+    response.liking_users = new EggsBook.Collections.Users(response.liking_users);
     return response;
   },
 
